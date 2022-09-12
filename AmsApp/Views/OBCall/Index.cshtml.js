8ae019b158a5901a74e23d97414decf956b43d05
Srinivas Koneti <konetidev@outlook.com>
2022-09-12 14:30:32 +0530

    call js

--- AmsApp/Views/OBCall/Index.cshtml.js
+++ AmsApp/Views/OBCall/Index.cshtml.js
@@ -2,3 +2,9 @@
 	
 });
 
+$("#btnCall").on('click', function () {
+    var leadid = $("#btnCall").attr("data");
+    localStorage.setItem("presentnumber", leadid);
+    var callUrl = myurl + 'OBCall/OnCall/' + leadid;
+    window.location.href = callUrl;
+});
