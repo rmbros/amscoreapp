70b3c9ca67fc4e40b563ec28753afe28dc23fc5b
Srinivas Koneti <konetidev@outlook.com>
2022-09-16 20:47:42 +0530

    Time Waste

--- AmsApp/Dto/OB/LeadCountDto.cs
+++ AmsApp/Dto/OB/LeadCountDto.cs
@@ -17,5 +17,22 @@ namespace AmsApp.Dto
         {
             get => TotalCalls == 0 ? 0 : Math.Round(TotalTime / (TotalCalls * 1.0), 2);
         }
+        public int TimeWasted
+        {
+            get {
+                DateTime startTime = DateTime.Now.Date.AddHours(9).AddMinutes(30);                
+                DateTime dayend = DateTime.Now.Date.AddHours(18).AddMinutes(30);
+                DateTime endTime = dayend.Subtract(DateTime.Now).TotalSeconds < 0 ? dayend : DateTime.Now;
+                TimeSpan span = endTime.Subtract(startTime);
+                if (endTime.Hour > 14)
+                {
+                    return Convert.ToInt32(span.TotalMinutes) - TotalTime - 30; //30min lunch break
+                }
+                else
+                {
+                    return Convert.ToInt32(span.TotalMinutes) - TotalTime;
+                }
+            }
+        }
     }
 }
